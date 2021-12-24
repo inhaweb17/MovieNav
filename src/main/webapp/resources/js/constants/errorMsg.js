4b7f46123c6d8bdc8c0407a87ad71f0c452346d6
liswkjts <liswktjs@gmail.com>
2021-12-24 20:25:42 +0900

    feat: 에러 상황에 따른 문구 정리 errorMsg에 에러 사항에 따른 문구와 케이스를 errorMsg에 상수로 정리

---
+++ src/main/webapp/resources/js/constants/errorMsg.js
@@ -0,0 +1,7 @@
+export const EMPTY_ERROR = 'empty-error';
+export const DUPLICATE_ID_ERROR = 'duplicate-id-error';
+
+export const ERRORMSG = {
+		'empty-error': '빈값을 입력하셨습니다 다시 입력해 주세요',
+		'duplicate-id-error': '이미 등록된 아이디 입니다 다른 아이디로 입력해 주세요'
+}
