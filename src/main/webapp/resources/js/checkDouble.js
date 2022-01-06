e706bcdb61114511f258536e4c20f574609409f4
soohy <soohyun120@naver.com>
2022-01-06 15:13:16 +0900

    UserController에 registerView 추가

--- src/main/webapp/resources/js/checkDouble.js
+++ src/main/webapp/resources/js/checkDouble.js
@@ -3,7 +3,7 @@ const user_id = document.querySelector('.user_id');
 
 function checkDobule(e){
 	const userId = user_id.value;
-	location.href=`user/idCheck?id=${userId}`;
+	location.href=`user/idCheck?userId=${userId}`;
 }
 
 confirmbtn.addEventListener("click",checkDouble);
