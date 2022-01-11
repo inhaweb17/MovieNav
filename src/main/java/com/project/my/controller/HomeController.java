b16bc4cdd00390768984e77df83e496f1741fa09
soohy <soohyun120@naver.com>
2022-01-11 09:42:36 +0900

    HomeController, CallAPI 수정

--- src/main/java/com/project/my/controller/HomeController.java
+++ src/main/java/com/project/my/controller/HomeController.java
@@ -3,15 +3,20 @@ package com.project.my.controller;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.project.my.dto.FilmDto;
+import com.project.my.dto.ReviewDto;
+import com.project.my.service.review.ReviewService;
 import com.project.my.util.CallAPI;
 
 /**
@@ -22,12 +27,16 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
+	@Autowired
+	ReviewService reviewService;
+	
+	CallAPI api = new CallAPI();
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) throws Exception {
 		logger.info("get home");
 		
 		// 일별 박스오피스
-		CallAPI api = new CallAPI();
 		List<String> boxoffice = api.callBoxOfiice();
 		List<FilmDto> filmlist = api.callFilmList(boxoffice);
 		
@@ -42,7 +51,8 @@ public class HomeController {
 		model.addAttribute("poster", poster);
 		
 		//전체 리뷰 - 최신순
-		
+		List<ReviewDto> reviewlist = reviewService.selectAll();
+		model.addAttribute("reviewlist", reviewlist);
 	
 		return "home";
 	}
@@ -65,17 +75,44 @@ public class HomeController {
 	
 	// 개봉예정작 화면
 	@RequestMapping(value = "/readyMovie", method = RequestMethod.GET)
-	public String readyMovie() throws Exception {
+	public String readyMovie(Model model) throws Exception {
 		logger.info("get readyMovie");
+		
+		List<String> titlelist = api.callNewFilm();
+		
+		List<FilmDto> filmlist = api.callFilmList(titlelist);
+		
+		model.addAttribute("filmlist", filmlist);
 		
 		return "readyMovie";
 	}
 	
 	// 장르별보기 화면
 	@RequestMapping(value = "/genreView", method = RequestMethod.GET)
-	public String genreView() throws Exception {
+	public String genreView(Model model) throws Exception {
 		logger.info("get genreView");
 		
+		Map<String,List<FilmDto>> genrelist = api.getGenreList();
+		
+		for(String genre : genrelist.keySet()) {
+			List<FilmDto> result = api.callFilmByGenre(genre);
+			genrelist.replace(genre, result);
+		}
+		
+		model.addAttribute("genrelist", genrelist);
+		
 		return "genreView";
+	}
+	
+	// 검색결과 화면
+	@RequestMapping(value = "/searchView", method = RequestMethod.GET)
+	public String searchView(@RequestParam("search") String search, Model model) throws Exception {
+		logger.info("get searchView");
+			
+		List<FilmDto> searchlist = api.getSearchList(search);
+			
+		model.addAttribute("searchlist", searchlist);
+		
+		return "searchView";
 	}
 }
